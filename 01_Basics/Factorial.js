94a9fcea4a35df9e3034dbd44f0c249997c724c0
Devendra Vishwakarma <166695417+ImDevendraVishwakarma@users.noreply.github.com>
2024-04-15 10:18:27 +0000

    done

---
+++ 01_Basics/Factorial.js
@@ -0,0 +1,9 @@
+let number = 5;
+let factorial = 1;
+
+
+for(let i = number; i > 0; i--){
+    factorial = factorial * i;
+}
+
+console.log(factorial);
--- 01_Basics/KilometersToMiles.js
+++ 01_Basics/KilometersToMiles.js
@@ -3,8 +3,4 @@ const milesIn1KM = 0.621371;
 
 let miles = kilometers * milesIn1KM;
 
-console.log(miles);const milesIn1KM = 0.621371;
-
-let miles = kilometers * milesIn1KM;
-
 console.log(miles);
